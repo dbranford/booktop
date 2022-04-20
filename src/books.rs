2866cab082d25cbe8b94b9280a848ba255d73382
dbranford <25404972+dbranford@users.noreply.github.com>
2022-04-20 20:40:25 +0100

    v0.0.1

---
+++ src/books.rs
@@ -0,0 +1,98 @@
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::fmt;
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Eq, PartialEq, Debug, Deserialize, Serialize)]
+enum Read {
+    Read,
+    Reading,
+    Stopped,
+    Unread,
+}
+
+impl fmt::Display for Read {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Read::Read => write!(f, "Read"),
+            Read::Reading => write!(f, "Reading"),
+            Read::Stopped => write!(f, "Stopped"),
+            Read::Unread => write!(f, "Unread"),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Debug, Deserialize, Serialize)]
+pub struct Book {
+    title: String,
+    author: String,
+    read: Read,
+}
+
+impl Book {
+    pub fn new(title: String, author: String) -> Book {
+        Book {
+            title,
+            author,
+            read: Read::Unread,
+        }
+    }
+    pub fn start(&mut self) {
+        self.read = Read::Reading
+    }
+    pub fn finish(&mut self) {
+        self.read = Read::Read
+    }
+    pub fn stop(&mut self) {
+        self.read = Read::Stopped
+    }
+    pub fn reset(&mut self) {
+        self.read = Read::Unread
+    }
+}
+
+impl fmt::Display for Book {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}---{:?} ({})", self.title, self.author, self.read)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Bookcase {
+    name: String,
+    pub books: BTreeMap<usize, Book>,
+}
+
+impl Bookcase {
+    pub fn new() -> Bookcase {
+        Bookcase {
+            name: "Bookcase".to_string(),
+            books: BTreeMap::new(),
+        }
+    }
+    pub fn open(path: &PathBuf) -> Bookcase {
+        let _file = File::open(path).expect("Could not open file");
+        serde_json::from_reader(_file).expect("Couldn't extract bookcase")
+    }
+    pub fn close(&self, path: &PathBuf) -> () {
+        let _file = File::create(path).expect("Could not open file");
+        serde_json::to_writer(_file, self).expect("Could not write to file");
+    }
+    pub fn list(&mut self) -> () {
+        println!("Bookcase: {}", self.name);
+        println!("========================================");
+        for (id, bk) in &self.books {
+            println!("{}: {}", id, bk);
+        }
+    }
+    pub fn add_book(&mut self, title: String, author: String) -> () {
+        self.books.insert(
+            self.books.keys().max().unwrap() + 1,
+            Book::new(title, author),
+        );
+    }
+    pub fn remove_book(&mut self, id: usize) -> () {
+        self.books.remove(&id);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,89 @@
+use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::path::PathBuf;
+mod books;
+
+#[derive(Debug, Parser)]
+#[clap(about = "A basic tracker for books", long_about = None)]
+struct Cli {
+    #[clap(long, short)]
+    /// File containing existing bookcase
+    file: Option<PathBuf>,
+
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Add a book
+    Add { title: String, author: String },
+    /// List all books
+    List {},
+    /// Initialise bookcase file
+    Init { path: PathBuf },
+    /// Remove book
+    Remove { id: usize },
+    /// Start reading a book
+    Start { id: usize },
+    /// Finish reading a book
+    Finish { id: usize },
+    /// Pause reading a book
+    Stop { id: usize },
+    /// Return a book to unread
+    Reset { id: usize },
+}
+
 fn main() {
+    let args = Cli::parse();
+
+    let mut file_path = args.file;
+
+    let mut books = match &file_path {
+        Some(path) => books::Bookcase::open(path),
+        None => books::Bookcase::new(),
+    };
+
+    match args.command {
+        // Bookcase operations
+        Commands::Add { title, author } => {
+            books.add_book(title, author);
+        }
+        Commands::Init { path } => {
+            File::create(&path).expect("Could not create file");
+            file_path = Some(path);
+        }
+        Commands::List {} => {
+            books.list();
+        }
+        Commands::Remove { id } => {
+            books.remove_book(id);
+        }
+        // Book operations
+        Commands::Finish { id } => {
+            if let Some(book) = books.books.get_mut(&id) {
+                book.finish();
+            }
+        }
+        Commands::Start { id } => {
+            if let Some(book) = books.books.get_mut(&id) {
+                book.start();
+            }
+        }
+        Commands::Reset { id } => {
+            if let Some(book) = books.books.get_mut(&id) {
+                book.reset()
+            }
+        }
+        Commands::Stop { id } => {
+            if let Some(book) = books.books.get_mut(&id) {
+                book.stop()
+            }
+        }
+    }
+
+    match &file_path {
+        Some(path) => books.close(path),
+        None => (),
+    };
 }
